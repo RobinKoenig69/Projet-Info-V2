a5d503db089e2e915a2253feb8321690d1bc28f3
DB9ga <robin.koenig@edu.ece.fr>
2024-05-20 21:48:18 +0200

    page de connexion finie avec le responsive pour téléphone, il manque encore le php et la liaison avec la BDD

--- templates/Connexion/Connexion.php
+++ templates/Connexion/Connexion.php
@@ -3,41 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <title>BlaBla Omnes</title>
-    <div class="logo">
     <link rel="stylesheet" href="Connexion.css">
-</div>
 </head>
 <body>
-    <div class="main-container">
-        <div class="top-bar">
-            <div class="logo-container">
-                <div class="logo-background">
-                    <img src="../../../images/Logo_omnes.png" alt="Logo" />
-                </div>
-            <div class="menu-icon">
-                <span></span>
-                <span></span>
-                <span></span>
+<div class="main-container">
+    <div class="top-bar">
+        <div class="logo-container">
+            <div class="logo-background">
+                <img src="../../../images/Logo_omnes.png" alt="Logo" />
             </div>
         </div>
-        <div class="bottom-bar"></div>
-        <div class="avatar"></div>
-        <img src="../../images/imagevoiture.png" alt="Placeholder image" class="placeholder-image"/>
-            <div class="entry">
-                <div class="overlap">
-                    <input type="email" name="email" class="text-input" placeholder="E-mail" required>
-                </div>
-                <div class="overlap-3">
-                    <input type="password" name="pwd" class="text-input" placeholder="Mot de passe" required>
-                </div>
-                <div class="se-connecter">
-                    <button type="submit" class="overlap-group-2">
-                        <div class="rectangle-2"></div>
-                        <div class="text-wrapper-5">Suivant</div>
-                    </button>
-                </div>
-            </div>
-        <div class="small-circle"></div>
+        <div class="menu-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
     </div>
+    <div class="bottom-bar"></div>
+    <div class="avatar"></div>
+    <img src="../../images/imagevoiture.png" alt="Placeholder image" class="placeholder-image"/>
+    <div class="small-circle"></div>
+    <div class="entry">
+        <div class="connexion">
+            <div>Connectez-vous :</div>
+        </div>
+        <div class="overlap">
+            <input type="email" name="email" class="text-input" placeholder="E-mail" required>
+        </div>
+        <div class="overlap-2">
+            <input type="password" name="pwd" class="text-input" placeholder="Mot de passe" required>
+        </div>
+        <div class="se-connecter">
+            <button type="submit" class="overlap-group-2">
+                <div class="rectangle-2"></div>
+                <div class="text-wrapper-5">Suivant</div>
+            </button>
+        </div>
+    </div>
+</div>
 </body>
 </html>
